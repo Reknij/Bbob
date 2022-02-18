333da4eace51450dd005b45950bf71d22b4a1631
JinkerLeong <jinker2003@gmail.com>
2022-02-18 23:29:12 +0800

    move it to Bbob.Plugin

---
+++ src/Bbob.Plugin/PluginJson.cs
@@ -0,0 +1,9 @@
+namespace Bbob.Plugin;
+
+public class PluginJson
+{
+    public string? name{get;set;}
+    public string? description{get;set;}
+    public string? author{get;set;}
+    public string? repository{get;set;}
+}
