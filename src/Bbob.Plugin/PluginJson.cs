6d6f510a80a2d64cc0b0f9368d09ebc974aec1d0
JinkerLeong <jinker2003@gmail.com>
2022-02-21 19:50:01 +0800

    auto assign default data, add entry section

--- src/Bbob.Plugin/PluginJson.cs
+++ src/Bbob.Plugin/PluginJson.cs
@@ -3,7 +3,14 @@ namespace Bbob.Plugin;
 public class PluginJson
 {
     public string? name{get;set;}
-    public string? description{get;set;}
-    public string? author{get;set;}
+    public string description{get;set;}
+    public string author{get;set;}
     public string? repository{get;set;}
+    public string entry{get;set;}
+    public PluginJson()
+    {
+        entry = "MainPlugin.dll";
+        description = "No description...";
+        author = "Unknown author";
+    }
 }
