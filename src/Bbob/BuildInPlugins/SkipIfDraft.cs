a8eaa33e758ff5db72ed14ac7bad5faf3bdb4a8a
JinkerLeong <jinker2003@gmail.com>
2022-02-23 18:01:56 +0800

    add SkipIfDraft plugin

---
+++ src/Bbob/BuildInPlugins/SkipIfDraft.cs
@@ -0,0 +1,21 @@
+using Bbob.Plugin;
+
+namespace Bbob.Main.BuildInPlugin;
+
+public class SkipIfDraft : IPlugin
+{
+    public void GenerateCommand(string filePath, string distribution, GenerationStage stage)
+    {
+        if (stage == GenerationStage.Process)
+        {
+            if (PluginHelper.getRegisteredObject<dynamic>("article", out dynamic? article))
+            {
+                if (article == null) return;
+                if (Extensions.IsPropertyExists<bool>(article, "draft", out bool value) && value)
+                {
+                    PluginHelper.ExecutingCommandResult = new CommandResult("It is draft.", CommandOperation.Skip);
+                }
+            }
+        }
+    }
+}
