9fb8009e5622ca787de343273c28310db2854b6f
JinkerLeong <jinker2003@gmail.com>
2022-02-27 13:02:08 +0800

    generate sitemap html

--- src/Bbob/BuildInPlugins/SitemapGenerator.cs
+++ src/Bbob/BuildInPlugins/SitemapGenerator.cs
@@ -45,15 +45,19 @@ public class SitemapGenerator : IPlugin
     }
     public void CommandComplete(Commands commands)
     {
-        if (commands == Commands.GenerateCommand && distribution != null)
+        if (commands == Commands.GenerateCommand && distribution != null && articlesUrl.Count > 0)
         {
             string baseUrl = PluginHelper.getRegisteredObjectNoNull<string>("config.baseUrl");
             string indexFile = Path.Combine(distribution, "index.html");
             string script = "<script>!function(n){var a;'/'===n.search[1]&&(a=n.search.slice(1).split('&').map(function(n){return n.replace(/~and~/g,'&')}).join('?'),window.history.replaceState(null,null,n.pathname.slice(0,-1)+a+n.hash))}(window.location);</script>";
+            string add = "<a href=\"/itissm.html\" hidden>Sitemap html</a>";
             string indexPlain = File.ReadAllText(indexFile);
-            string pattern = @"<head>(.*)</head>";
-            string replacement = $"<head>{script}$1</head>";
-            indexPlain = Regex.Replace(indexPlain, pattern, replacement, RegexOptions.Singleline);
+            string patternHead = @"<head>(.*)</head>";
+            string patternBody = @"<body>(.*)</body>";
+            string replacement1 = $"<head>{script}$1</head>";
+            string replacement2 = $"<body>{add}$1</body>";
+            indexPlain = Regex.Replace(indexPlain, patternHead, replacement1, RegexOptions.Singleline);
+            indexPlain = Regex.Replace(indexPlain, patternBody, replacement2, RegexOptions.Singleline);
             File.WriteAllText(indexFile, indexPlain);
             generateSitemap(distribution);
             generateRobotTxt(distribution);
@@ -79,6 +83,43 @@ public class SitemapGenerator : IPlugin
         }
         aurl.AddRange(articlesUrl);
         File.WriteAllLines(sitemap, aurl);
+        generateSitemapHtml(distribution, aurl);
+    }
+
+    private void generateSitemapHtml(string distribution, List<string> addressList)
+    {
+        string sitemap = Path.Combine(distribution, "itissm.html");
+        string metas = "<meta charset=\"UTF-8\"/>"+
+                        "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\" />"+
+                        "<meta name=\"robots\" content=\"noindex\">";
+        string style = "<style>li{margin: 15px auto}</style>";
+        UnsortedList unsortedList = new UnsortedList();
+        foreach (string address in addressList)
+        {
+            unsortedList.AddItem($"<a href=\"{address}\">address</a>");
+        }
+        string html = $"<!DOCTYPE html><html lang=\"en\"><head>{metas}<title>Sitemap Html</title>{style}</head><body>{unsortedList}</body></html>";
+        File.WriteAllText(sitemap, html);
+    }
+
+    class UnsortedList
+    {
+        string html;
+        bool Format {get;set;} = false;
+        public UnsortedList()
+        {
+            html = "<ul>";
+            if (Format) html += '\n';
+        }
+        public void AddItem(string item)
+        {
+            html += $"<li>{item}</li>";
+            if (Format) html += '\n';
+        }
+        public override string ToString()
+        {
+            return html + "</ul>";
+        }
     }
 
     public record class Config
