2dfd9833ce3cf9361ac2a27ea20b136ac8323b71
JinkerLeong <jinker2003@gmail.com>
2022-02-27 21:22:12 +0800

    optimzie functions

--- src/Bbob.Plugin/Extensions.cs
+++ src/Bbob.Plugin/Extensions.cs
@@ -4,12 +4,12 @@ public static class Extensions
 {
     public static bool IsPropertyExists(object obj, string property)
     {
-        IsPropertyExistsMain(obj, property, out bool exists, out object? pObj);
+        bool exists = IsPropertyExistsMain(obj, property, out object? pObj);
         return exists;
     }
     public static bool IsPropertyExists<T>(object obj, string property)
     {
-        IsPropertyExistsMain(obj, property, out bool exists, out object? pObj);
+        bool exists = IsPropertyExistsMain(obj, property, out object? pObj);
         if (exists && pObj?.GetType() is T)
         {
             return true;
@@ -18,7 +18,7 @@ public static class Extensions
     }
     public static bool IsPropertyExists<T>(object obj, string property, out T? propertyWithType)
     {
-        IsPropertyExistsMain(obj, property, out bool exists, out object? pObj);
+        bool exists = IsPropertyExistsMain(obj, property, out object? pObj);
         if (exists && pObj?.GetType() == typeof(T))
         {
             propertyWithType = (T)pObj;
@@ -27,16 +27,14 @@ public static class Extensions
         propertyWithType = default(T);
         return false;
     }
-    public static void IsPropertyExistsMain(object obj, string property, out bool exists, out object? pObj)
+    private static bool IsPropertyExistsMain(object obj, string property, out object? pObj)
     {
-        if (obj is System.Dynamic.ExpandoObject)
+        if (obj is IDictionary<string, object?> tar)
         {
-            var tar = (IDictionary<string, object>)obj;
             if (tar.ContainsKey(property))
             {
-                exists = true;
                 pObj = tar[property];
-                return;
+                return true;
             }
         }
         var properties = ((object)obj).GetType().GetProperties();
@@ -44,12 +42,11 @@ public static class Extensions
         {
             if (p.Name == property)
             {
-                exists = true;
                 pObj = p.GetValue(obj);
-                return;
+                return true;
             }
         }
-        exists = false;
         pObj = null;
+        return false;
     }
 }
