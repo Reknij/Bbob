09e99cd18f1a6fea2d465800b067cb8ed44859c4
JinkerLeong <jinker2003@gmail.com>
2022-03-03 15:42:21 +0800

    support category use folder name

--- src/Bbob/BuildInPlugins/CategoryProcess.cs
+++ src/Bbob/BuildInPlugins/CategoryProcess.cs
@@ -7,6 +7,11 @@ public class CategoryProcess : IPlugin
 {
     string distribution = "";
 
+    public void InitCommand()
+    {
+        PluginHelper.savePluginJsonConfig<MyConfig>(new MyConfig("default"));
+    }
+
     public void GenerateCommand(string filePath, string distribution, GenerationStage stage)
     {
         if (stage == GenerationStage.Initialize)
@@ -25,19 +30,45 @@ public class CategoryProcess : IPlugin
             PluginHelper.getRegisteredObject<dynamic>("article", out dynamic? article);
             if (article == null) return;
             if (blog == null) return;
-            if (Extensions.IsPropertyExists<List<object>>(article, "categories", out List<object> categories))
+            PluginHelper.getPluginJsonConfig<MyConfig>(out MyConfig? tar);
+            MyConfig config = tar ?? new MyConfig("default");
+            switch (config.mode)
             {
-                foreach (var t in categories)
-                {
-                    if (t is string text)
+                case "default":
                     {
+                        if (Extensions.IsPropertyExists<List<object>>(article, "categories", out List<object> categories))
+                        {
+                            foreach (var t in categories)
+                            {
+                                if (t is string text)
+                                {
+                                    if (!blog.categories.ContainsKey(text))
+                                    {
+                                        blog.categories.Add(text, new List<dynamic> { article });
+                                    }
+                                    else blog.categories[text].Add(article);
+                                }
+                            }
+                        }
+                        break;
+                    }
+                case "folder":
+                    {
+                        string? text = Directory.GetParent(filePath)?.Name;
+                        if (text == null)
+                        {
+                            PluginHelper.printConsole($"Parent folder of file path '{filePath}' is null, can't get the name of category.");
+                        }
                         if (!blog.categories.ContainsKey(text))
                         {
                             blog.categories.Add(text, new List<dynamic> { article });
                         }
                         else blog.categories[text].Add(article);
+                        break;
                     }
-                }
+                default:
+                    PluginHelper.printConsole("config.mode unknown!");
+                    break;
             }
         }
     }
@@ -59,4 +90,6 @@ public class CategoryProcess : IPlugin
             }
         }
     }
+
+    public record class MyConfig(string mode);
 }
