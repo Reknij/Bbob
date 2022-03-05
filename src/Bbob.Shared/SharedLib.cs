b0794a5746a6f0e7df620c88947ecd2d054b916a
JinkerLeong <jinker2003@gmail.com>
2022-03-05 19:53:54 +0800

    add UrlHelper

--- src/Bbob.Shared/SharedLib.cs
+++ src/Bbob.Shared/SharedLib.cs
@@ -126,4 +126,25 @@ public static class SharedLib
             Console.Write('\n');
         }
     }
+
+    public static class UrlHelper
+    {
+        public static string UrlCombine(string url1, string url2) //https://stackoverflow.com/questions/372865/path-combine-for-urls/2806717#2806717
+        {
+            if (url1.Length == 0)
+            {
+                return url2;
+            }
+
+            if (url2.Length == 0)
+            {
+                return url1;
+            }
+
+            url1 = url1.TrimEnd('/', '\\');
+            url2 = url2.TrimStart('/', '\\');
+
+            return string.Format("{0}/{1}", url1, url2);
+        }
+    }
 }
