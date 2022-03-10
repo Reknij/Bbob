fcaf0b4ffbc473bfcf63a482cd23c5853583e7b6
JinkerLeong <jinker2003@gmail.com>
2022-03-10 12:24:36 +0800

    support extraLinks and optimize footer

--- src/BbobDocTheme/src/components/Footer.vue
+++ src/BbobDocTheme/src/components/Footer.vue
@@ -4,20 +4,32 @@ import Bbob from '../../../Bbob/JSApi/Bbob'
 </script>
 
 <template>
-    <p class="copyright">
-        © 2022 {{Bbob.meta.author}} Powered by <a class="beautifulLink" href="https://github.com/Reknij/Bbob">Bbob</a> & documents
-    </p>
+    <div style="text-align: center;">
+        <span class="copyright" v-html="Bbob.meta.copyright"></span>
+         <br>
+        <el-tag color="white" class="extraLink" v-if="Bbob.meta.extra.extraLinks" v-for="(link, i) in Bbob.meta.extra.extraLinks" :key="i">
+            <a :href="link.address">{{link.name}}</a>
+        </el-tag>
+    </div>
 </template>
 
 <style>
-.copyright{
+.extraLink {
+    margin: 5px 0px 2px 2px;
+}
+.extraLink a {
+    text-decoration: none;
+    color: black;
+}
+.copyright {
+    margin: auto 0px;
     text-align: center;
 }
-.beautifulLink{
+.bbob-project-link {
     text-decoration: none;
-    color: #8300FF;
+    color: #8300ff;
 }
-.beautifulLink:hover{
+.bbob-project-link:hover {
     text-decoration: underline;
 }
 </style>
