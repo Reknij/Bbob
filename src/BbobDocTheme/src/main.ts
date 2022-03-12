60c97993a3a94857c8c616928b1629463d47c3f8
JinkerLeong <jinker2003@gmail.com>
2022-03-12 17:14:08 +0800

    migrate BbobDocTheme

--- src/BbobDocTheme/src/main.ts
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import { router } from './router';
-import ElementPlus from 'element-plus';
-
-let app = createApp(App);
-app.use(router);
-app.use(ElementPlus);
-app.mount('#app')
