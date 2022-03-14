8daf7cfc29c31f79e2618e60bfcdc49bfd639b55
JinkerLeong <jinker2003@gmail.com>
2022-03-14 13:25:39 +0800

    lock and update version.

--- src/BbobDefaultTheme/vite.config.ts
+++ src/BbobDefaultTheme/vite.config.ts
@@ -11,18 +11,18 @@ export default defineConfig({
         {
           name: 'vue',
           var: 'Vue',
-          path: 'https://unpkg.com/vue@3.2.25/dist/vue.runtime.global.prod.js'
+          path: 'https://unpkg.com/vue@3.2.31/dist/vue.runtime.global.prod.js'
         },
         {
           name: 'vue-router',
           var: 'VueRouter',
-          path: 'https://unpkg.com/vue-router@4.0.12/dist/vue-router.global.prod.js'
+          path: 'https://unpkg.com/vue-router@4.0.14/dist/vue-router.global.prod.js'
         },
         {
           name: 'element-plus',
           var: 'ElementPlus',
-          path: 'https://unpkg.com/element-plus',
-          css: 'https://unpkg.com/element-plus/dist/index.css'
+          path: 'https://unpkg.com/element-plus@2.1.0',
+          css: 'https://unpkg.com/element-plus@2.1.0/dist/index.css'
         },
       ]
     })
