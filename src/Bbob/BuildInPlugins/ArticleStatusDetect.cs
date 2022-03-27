459f6ee954068fd67608a31de66df4bd1e52927a
JinkerLeong <jinker2003@gmail.com>
2022-03-27 15:01:49 +0800

    SkipIfDraft refactoring to ArticleStatusDetect

---
+++ src/Bbob/BuildInPlugins/ArticleStatusDetect.cs
@@ -0,0 +1,26 @@
+using Bbob.Plugin;
+
+namespace Bbob.Main.BuildInPlugin;
+
+[PluginCondition("*", PluginOrder = PluginOrder.AfterMe)]
+public class ArticleStatusDetect : IPlugin
+{
+    public void GenerateCommand(string filePath, GenerationStage stage)
+    {
+        if (stage == GenerationStage.Process)
+        {
+            if (PluginHelper.getRegisteredObject<dynamic>("article", out dynamic? article))
+            {
+                if (article == null) return;
+                if (Extensions.IsPropertyExists<bool>(article, "draft", out bool draft) && draft)
+                {
+                    PluginHelper.ExecutingCommandResult = new CommandResult("It is draft.", CommandOperation.Skip);
+                }
+                if (Extensions.IsPropertyExists<bool>(article, "important", out bool important) && important)
+                {
+                    PluginHelper.ExecutingCommandResult = new CommandResult("It is important article. Please complete it first.", CommandOperation.Stop);
+                }
+            }
+        }
+    }
+}
