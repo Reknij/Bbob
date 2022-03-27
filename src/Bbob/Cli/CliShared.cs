223d2800fcef8c9a959d17a57f3735c8e83a8774
JinkerLeong <jinker2003@gmail.com>
2022-03-27 19:38:02 +0800

    fix get free port bug

--- src/Bbob/Cli/CliShared.cs
+++ src/Bbob/Cli/CliShared.cs
@@ -29,16 +29,23 @@ public static class CliShared
 
     public static int GetAvailablePort(int startingPort)
     {
-        if (startingPort > ushort.MaxValue) throw new ArgumentException($"Can't be greater than {ushort.MaxValue}", nameof(startingPort));
-        var ipGlobalProperties = IPGlobalProperties.GetIPGlobalProperties();
+        if (startingPort < 1024 || startingPort > 49151) throw new Exception("startingPort must in 1024 - 49151");
+        var ipProperties = IPGlobalProperties.GetIPGlobalProperties();
 
-        var connectionsEndpoints = ipGlobalProperties.GetActiveTcpConnections().Select(c => c.LocalEndPoint);
-        var tcpListenersEndpoints = ipGlobalProperties.GetActiveTcpListeners();
-        var udpListenersEndpoints = ipGlobalProperties.GetActiveUdpListeners();
-        var portsInUse = connectionsEndpoints.Concat(tcpListenersEndpoints)
-                                             .Concat(udpListenersEndpoints)
-                                             .Select(e => e.Port);
+        var usedPorts =
+            ipProperties.GetActiveTcpConnections()
+                .Where(connection => connection.State == TcpState.Listen)
+                .Select(connection => connection.LocalEndPoint)
+                .Concat(ipProperties.GetActiveTcpListeners())
+                .Concat(ipProperties.GetActiveUdpListeners())
+                .Select(endpoint => endpoint.Port)
+                .ToArray();
 
-        return Enumerable.Range(startingPort, ushort.MaxValue - startingPort + 1).Except(portsInUse).FirstOrDefault();
+        while (startingPort <= 49151)
+        {
+            if (!usedPorts.Contains(startingPort)) return startingPort;
+            else startingPort++;
+        }
+        throw new Exception($"All local TCP ports between {startingPort} and {49151} are currently in use.");
     }
 }
