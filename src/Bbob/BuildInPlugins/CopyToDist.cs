2b7eae90dad2e72f8a73a76959ea127ddc46aab3
JinkerLeong <jinker2003@gmail.com>
2022-04-02 11:49:46 +0800

    refactoring CopyToDist

--- src/Bbob/BuildInPlugins/CopyToDist.cs
+++ src/Bbob/BuildInPlugins/CopyToDist.cs
@@ -2,16 +2,46 @@ using Bbob.Plugin;
 
 namespace Bbob.Main.BuildInPlugin;
 
-public class CopyToDist: IPlugin
+public class CopyToDist : IPlugin
 {
-    static readonly string myFiles = Path.Combine(PluginHelper.CurrentDirectory, "MyFiles");
+    public void InitCommand()
+    {
+        if (!PluginHelper.isPluginJsonConfigExists())
+        {
+            PluginHelper.savePluginJsonConfig<MyConfig>(new MyConfig());
+            PluginHelper.printConsole("Initialize config file.");
+        }
+        else
+        {
+            PluginHelper.printConsole("Already exists config.");
+        }
+    }
+
+    static readonly string publicPath = Path.Combine(PluginHelper.CurrentDirectory, "public");
     public Action? CommandComplete(Commands commands)
     {
-        if (commands == Commands.GenerateCommand && Directory.Exists(myFiles) && Directory.Exists(PluginHelper.DistributionDirectory))
+        if (commands == Commands.GenerateCommand && Directory.Exists(publicPath) && Directory.Exists(PluginHelper.DistributionDirectory))
         {
-            PluginHelper.printConsole("Exists 'MyFiles' folder, will copy all files and directories of 'MyFiles' to distribution.");
-            Shared.SharedLib.DirectoryHelper.CopyDirectory(myFiles, PluginHelper.DistributionDirectory);
+            PluginHelper.getPluginJsonConfig(out MyConfig? config);
+            config ??= new MyConfig();
+            PluginHelper.printConsole("Exists 'public' folder, will copy all files and directories of 'public' to distribution.");
+            Shared.SharedLib.DirectoryHelper.CopyDirectory(publicPath, PluginHelper.DistributionDirectory, string.Empty, true, config.overwrite);
+            HashSet<string> already = new HashSet<string>() { publicPath };
+            foreach (var item in config.files)
+            {
+                if (!already.Contains(item))
+                {
+                    PluginHelper.printConsole($"Will copy files of '{item}' too.");
+                    Shared.SharedLib.DirectoryHelper.CopyDirectory(item, PluginHelper.DistributionDirectory, string.Empty, true, config.overwrite);
+                }
+            }
         }
         return null;
+    }
+
+    public class MyConfig
+    {
+        public bool overwrite { get; set; } = true;
+        public string[] files { get; set; } = Array.Empty<string>();
     }
 }
