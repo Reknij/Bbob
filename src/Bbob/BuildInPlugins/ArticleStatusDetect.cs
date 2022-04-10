279ac102e0fe22220989bb625bc2d834b495b190
JinkerLeong <jinker2003@gmail.com>
2022-04-10 15:46:58 +0800

    add notice status and optimize order to detect

--- src/Bbob/BuildInPlugins/ArticleStatusDetect.cs
+++ src/Bbob/BuildInPlugins/ArticleStatusDetect.cs
@@ -13,13 +13,23 @@ public class ArticleStatusDetect : IPlugin
             if (PluginHelper.getRegisteredObject<dynamic>("article", out dynamic? article))
             {
                 if (article == null) return;
-                if (Extensions.IsPropertyExists<bool>(article, "draft", out bool draft) && draft)
-                {
-                    PluginHelper.ExecutingCommandResult = new CommandResult("It is draft.", CommandOperation.Skip);
-                }
                 if (Extensions.IsPropertyExists<bool>(article, "important", out bool important) && important)
                 {
                     PluginHelper.ExecutingCommandResult = new CommandResult("It is important article. Please complete it first.", CommandOperation.Stop);
+                }
+                else if (Extensions.IsPropertyExists<bool>(article, "draft", out bool draft) && draft)
+                {
+                    PluginHelper.ExecutingCommandResult = new CommandResult("It is draft.", CommandOperation.Skip);
+                }
+                else if (Extensions.IsPropertyExists<string>(article, "notice", out string? notice) && notice != null)
+                {
+                    PluginHelper.printConsole(notice, ConsoleColor.Yellow);
+                    ConsoleKeyInfo info = PluginHelper.readConsoleKey("Enter 'y' to continue generate this article or 'n' to skip it: ");
+                    while (info.Key != ConsoleKey.Y && info.Key != ConsoleKey.N)
+                    {
+                        info = PluginHelper.readConsoleKey("Please enter 'y' or 'n': ", false);
+                    };
+                    if (info.Key == ConsoleKey.N) PluginHelper.ExecutingCommandResult = new CommandResult(CommandOperation.Skip);
                 }
             }
         }
